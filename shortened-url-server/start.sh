943e4e34f12d8c3621fc45a9a7fe791ac8ac71eb
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-20 17:42:02 +0900

    chore: echo 삭제

--- shortened-url-server/start.sh
+++ shortened-url-server/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "Create docker image start"
-
 # 이미지 생성
 docker build -t shortened-url-server -f shortened-url-server/Dockerfile .
-
-echo "Create docker container start"
 
 # 이미지 실행
 docker-compose -f shortened-url-server/docker-compose.yml up -d
