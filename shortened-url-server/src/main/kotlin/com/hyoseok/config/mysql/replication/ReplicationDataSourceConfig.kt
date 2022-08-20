31ef42713e41731065e20429f81fb91a326a7bab
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-20 15:44:43 +0900

    chore: MySQL Replication DataSource 설정 작업

---
+++ shortened-url-server/src/main/kotlin/com/hyoseok/config/mysql/replication/ReplicationDataSourceConfig.kt
@@ -0,0 +1,65 @@
+package com.hyoseok.config.mysql.replication
+
+import com.hyoseok.config.mysql.replication.property.ReadProperty
+import com.hyoseok.config.mysql.replication.property.WriteProperty
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import javax.sql.DataSource
+
+@Configuration
+@EnableConfigurationProperties(value = [WriteProperty::class, ReadProperty::class])
+@Profile(value = ["prod"])
+class ReplicationDataSourceConfig(
+    private val writeProperty: WriteProperty,
+    private val readProperty: ReadProperty,
+) {
+
+    @Bean
+    fun writeDataSource(): DataSource = HikariDataSource().apply {
+        poolName = "WriteDataSourcePool"
+        driverClassName = writeProperty.driverClassName
+        jdbcUrl = writeProperty.jdbcUrl
+        minimumIdle = writeProperty.minimumIdle
+        maximumPoolSize = writeProperty.maximumPoolSize
+        maxLifetime = writeProperty.maxLifetime
+        connectionTimeout = writeProperty.connectionTimeout
+    }
+
+    @Bean
+    fun readDataSource(): DataSource = HikariDataSource().apply {
+        poolName = "ReadDataSourcePool"
+        driverClassName = readProperty.driverClassName
+        jdbcUrl = readProperty.jdbcUrl
+        minimumIdle = readProperty.minimumIdle
+        maximumPoolSize = readProperty.maximumPoolSize
+        maxLifetime = readProperty.maxLifetime
+        connectionTimeout = readProperty.connectionTimeout
+    }
+
+    @Bean
+    fun routingDataSource(): DataSource {
+        val dataSourceMap: MutableMap<Any, Any> = HashMap()
+        val orderWriteDataSource = writeDataSource()
+        val orderReadDataSource = readDataSource()
+
+        dataSourceMap[ReplicationDataSourceType.WRITE] = orderWriteDataSource
+        dataSourceMap[ReplicationDataSourceType.READ] = orderReadDataSource
+
+        val replicationRoutingDataSource = ReplicationRoutingDataSource()
+        replicationRoutingDataSource.setTargetDataSources(dataSourceMap)
+        replicationRoutingDataSource.setDefaultTargetDataSource(orderWriteDataSource)
+
+        return replicationRoutingDataSource
+    }
+
+    @Bean
+    @Primary
+    fun dataSource(): DataSource {
+        return LazyConnectionDataSourceProxy(routingDataSource())
+    }
+}
---
+++ shortened-url-server/src/main/kotlin/com/hyoseok/config/mysql/replication/ReplicationDataSourceType.kt
@@ -0,0 +1,6 @@
+package com.hyoseok.config.mysql.replication
+
+enum class ReplicationDataSourceType {
+    WRITE,
+    READ
+}
