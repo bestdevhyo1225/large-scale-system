f13479de5af18feb55b83a09d994ec433c9d2ec5
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-23 00:40:13 +0900

    style: ktlint 검사에 따른 코드 스타일 적용

--- order-payment-server/domain/src/main/kotlin/com/bestdev/order/entity/enums/OrderStatus.kt
+++ order-payment-server/domain/src/main/kotlin/com/bestdev/order/entity/enums/OrderStatus.kt
@@ -20,11 +20,11 @@ enum class OrderStatus(val label: String) : Cancelable, Failable {
     REFUND("주문 환불") {
         override fun isCanceled() = false
         override fun isFailed() = false
-    },
-    FAIL("주문 실패") {
-        override fun isCanceled() = false
-        override fun isFailed() = true
     };
+//    FAIL("주문 실패") {
+//        override fun isCanceled() = false
+//        override fun isFailed() = true
+//    };
 
     companion object {
         operator fun invoke(value: String) = OrderStatus.valueOf(value.uppercase())
