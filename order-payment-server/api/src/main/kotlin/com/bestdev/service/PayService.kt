462a67f4b82466afd0340bf24759ad8fe8c8cb16
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-24 08:54:59 +0900

    feat: PaymentService 클래스 생성

---
+++ order-payment-server/api/src/main/kotlin/com/bestdev/service/PayService.kt
@@ -0,0 +1,9 @@
+package com.bestdev.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class PayService {
+}
