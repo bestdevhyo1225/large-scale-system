17c871c9da702d3d65e7c22c1b55c457c2b1efb8
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-28 01:26:43 +0900

    refactor: longUrl 값을 sha-256 알고리즘을 통해 shortUrl 값을 생성하도록 리팩토링

--- shortened-url-server-ver3/command/src/main/kotlin/com/hyoseok/service/UrlService.kt
+++ shortened-url-server-ver3/command/src/main/kotlin/com/hyoseok/service/UrlService.kt
@@ -3,7 +3,7 @@ package com.hyoseok.service
 import com.hyoseok.config.RedissonUseType
 import com.hyoseok.entity.Url
 import com.hyoseok.repository.UrlRepository
-import com.hyoseok.utils.Base62Util
+import com.hyoseok.utils.Sha256Util
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -18,16 +18,11 @@ class UrlService(
 
     @Transactional
     fun create(longUrl: String): String {
-        val shortUrl = Base62Util.encode(value = System.currentTimeMillis())
-
-        return distributedLockService.executeWithLock(value = shortUrl, useType = RedissonUseType.SHORT_URL) {
-            urlRepository.findByLongUrl(longUrl = longUrl)?.let { return@executeWithLock it.shortUrl }
-
-            val url = Url(shortUrl = shortUrl, longUrl = longUrl)
-
-            urlRepository.save(url)
-
-            url.shortUrl
+        val shortUrl = Sha256Util.encode(value = longUrl)
+        return distributedLockService.executeWithLock(value = shortUrl, useType = RedissonUseType.URL) {
+            urlRepository.findByShortUrl(shortUrl = shortUrl)?.let { return@executeWithLock it.shortUrl }
+            urlRepository.save(url = Url(shortUrl = shortUrl, longUrl = longUrl))
+            shortUrl
         }
     }
 }
