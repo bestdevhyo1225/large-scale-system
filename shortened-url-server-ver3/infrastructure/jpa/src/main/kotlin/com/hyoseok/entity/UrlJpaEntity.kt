d7b890187fbb43c2d8959a1192387ed2804f80e6
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-27 18:39:05 +0900

    feat: 인덱스 추가

--- shortened-url-server-ver3/infrastructure/jpa/src/main/kotlin/com/hyoseok/entity/UrlJpaEntity.kt
+++ shortened-url-server-ver3/infrastructure/jpa/src/main/kotlin/com/hyoseok/entity/UrlJpaEntity.kt
@@ -8,10 +8,17 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Index
 import javax.persistence.Table
 
 @Entity
-@Table(name = "urls")
+@Table(
+    name = "urls",
+    indexes = [
+        Index(name = "ix_urls_short_url", columnList = "short_url"),
+        Index(name = "ix_urls_long_url", columnList = "long_url"),
+    ],
+)
 @DynamicUpdate
 class UrlJpaEntity private constructor(
     shortUrl: String,
