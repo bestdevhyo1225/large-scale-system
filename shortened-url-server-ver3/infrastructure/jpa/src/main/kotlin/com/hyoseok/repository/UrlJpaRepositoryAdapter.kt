cd113193847781cfa5c7da212283d8f3bb170bdb
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-27 13:26:54 +0900

    refactor: fallback 메서드 리팩토링

--- shortened-url-server-ver3/infrastructure/jpa/src/main/kotlin/com/hyoseok/repository/UrlJpaRepositoryAdapter.kt
+++ shortened-url-server-ver3/infrastructure/jpa/src/main/kotlin/com/hyoseok/repository/UrlJpaRepositoryAdapter.kt
@@ -1,15 +1,21 @@
 package com.hyoseok.repository
 
+import com.hyoseok.constants.CircuitBreakerName
 import com.hyoseok.entity.Url
 import com.hyoseok.entity.UrlJpaEntity
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import mu.KotlinLogging
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Repository
 @Transactional(readOnly = true)
 class UrlJpaRepositoryAdapter(
     private val urlJpaRepository: UrlJpaRepository,
 ) : UrlRepository {
+
+    private val logger = KotlinLogging.logger {}
 
     override fun save(url: Url) {
         urlJpaRepository.save(UrlJpaEntity(url = url))
@@ -20,4 +26,9 @@ class UrlJpaRepositoryAdapter(
 
     override fun findByShortUrl(shortUrl: String): Url? =
         urlJpaRepository.findByShortUrl(shortUrl = shortUrl)?.toDomainEntity()
+
+    private fun findTempUrl(exception: Exception): Url {
+        logger.info { "fallback: ${exception.localizedMessage}" }
+        return Url(id = 1, shortUrl = "shortUrl", longUrl = "longUrl", createdAt = LocalDateTime.now())
+    }
 }
