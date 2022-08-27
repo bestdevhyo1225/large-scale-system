eaa5045d148930e6cd4522a7d9815490992b08d9
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-28 01:25:23 +0900

    test: 단위 테스트 코드 추가

---
+++ shortened-url-server-ver3/common/src/test/kotlin/com/hyoseok/utils/Sha256UtilTests.kt
@@ -0,0 +1,25 @@
+package com.hyoseok.utils
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+internal class Sha256UtilTests : DescribeSpec(
+    {
+        describe("encode 메서드는") {
+            it("입력 값을 sha-256 알고리즘을 통해 암호화한다.") {
+                // given
+                val value1 = "https://hyos-dev-log.tistory.com/"
+                val value2 = "https://hyos-dev-log.tistory.com/"
+
+                // when
+                val result1 = Sha256Util.encode(value = value1)
+                val result2 = Sha256Util.encode(value = value2)
+
+                println(result1)
+
+                // then
+                result1.shouldBe(result2)
+            }
+        }
+    },
+)
