68363a209c5094422e48f46faeb44f5fa3993431
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-07 00:08:23 +0900

    chore: Blocking Call 코드를 검사하기 위한 BlockHound 의존성 모듈 추가

--- shortened-url-server-ver3/query-webflux/src/main/kotlin/com/hyoseok/QueryWebFluxApplication.kt
+++ shortened-url-server-ver3/query-webflux/src/main/kotlin/com/hyoseok/QueryWebFluxApplication.kt
@@ -8,7 +8,6 @@ import reactor.blockhound.BlockHound
 class QueryWebFluxApplication
 
 fun main(args: Array<String>) {
-    // 애플리케이션 내의 블로킹 코드롤 체크하고, 블로킹 코드가 존재할 경우 에러를 발생시킨다.
-    BlockHound.install()
+    BlockHound.install() // 애플리케이션 내의 블로킹 코드롤 체크하고, 블로킹 코드가 존재할 경우 에러를 발생시킨다.
     runApplication<QueryWebFluxApplication>(*args)
 }
