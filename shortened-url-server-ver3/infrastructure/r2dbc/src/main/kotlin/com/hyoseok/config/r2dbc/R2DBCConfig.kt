38c045eef5c91eca4f9c0f241e5f43617b7d9072
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-07 02:08:40 +0900

    chore: prod 환경에서만 사용할 수 있도록

--- shortened-url-server-ver3/infrastructure/r2dbc/src/main/kotlin/com/hyoseok/config/r2dbc/R2DBCConfig.kt
+++ shortened-url-server-ver3/infrastructure/r2dbc/src/main/kotlin/com/hyoseok/config/r2dbc/R2DBCConfig.kt
@@ -4,9 +4,11 @@ import io.r2dbc.spi.ConnectionFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.r2dbc.connection.R2dbcTransactionManager
 
 @Configuration
+@Profile("prod")
 class R2DBCConfig {
 
     @Bean
