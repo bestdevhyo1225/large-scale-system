9fb135e7f9a36ba19f10f1c71374e4348d870914
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-08 00:36:57 +0900

    chore: kotlinx-coroutines-reactor 의존성 모듈 추가

--- shortened-url-server-ver3/infrastructure/r2dbc/build.gradle.kts
+++ shortened-url-server-ver3/infrastructure/r2dbc/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
