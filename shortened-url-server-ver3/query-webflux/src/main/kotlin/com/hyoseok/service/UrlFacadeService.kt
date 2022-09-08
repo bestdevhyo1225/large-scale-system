4a197300db68274f087742adc14057da63c6f925
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-08 21:23:16 +0900

    feat: Cache 저장시 Coroutine, Dispatcher.IO 를 통해 비동기 저장 되도록 구현

--- shortened-url-server-ver3/query-webflux/src/main/kotlin/com/hyoseok/service/UrlFacadeService.kt
+++ shortened-url-server-ver3/query-webflux/src/main/kotlin/com/hyoseok/service/UrlFacadeService.kt
@@ -3,6 +3,9 @@ package com.hyoseok.service
 import com.hyoseok.config.RedisExpireTimes
 import com.hyoseok.config.RedisKeys
 import com.hyoseok.repository.UrlCacheNonBlockingRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
@@ -27,11 +30,13 @@ class UrlFacadeService(
 
         val longUrl: String = urlService.findLongUrl(encodedUrl = encodedUrl)
 
-        urlReactiveRedisRepository.set(
-            key = key,
-            value = longUrl,
-            expireDuration = Duration.ofSeconds(RedisExpireTimes.URLS),
-        )
+        CoroutineScope(context = Dispatchers.IO).launch {
+            urlReactiveRedisRepository.set(
+                key = key,
+                value = longUrl,
+                expireDuration = Duration.ofSeconds(RedisExpireTimes.URLS),
+            )
+        }
 
         return longUrl
     }
