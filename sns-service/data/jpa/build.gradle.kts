5677fbbcc60f1fc367a4e955ba03c4dfbdb96f75
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-10 03:02:30 +0900

    chore: jpa 모듈에 필요한 의존성 추가

---
+++ sns-service/data/jpa/build.gradle.kts
@@ -0,0 +1,29 @@
+apply(plugin = "kotlin-jpa")
+apply(plugin = "kotlin-allopen")
+
+allOpen {
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
+dependencies {
+    implementation(project(":sns-service:common"))
+    implementation(project(":sns-service:domain"))
+
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+    // resilience4j
+    implementation("io.github.resilience4j:resilience4j-spring-boot2:1.7.1")
+    implementation("io.github.resilience4j:resilience4j-retrofit:1.7.1")
+}
+
+kotlin.sourceSets.main {
+    setBuildDir("$buildDir")
+}
