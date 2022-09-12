1a9fd01cb7ed33e29ff4c6cca4ff1ca9e8889c7d
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-12 20:07:19 +0900

    feat: Redis HashTag Key 제거

--- sns-service/data/redis/src/main/kotlin/com/hyoseok/config/RedisKeys.kt
+++ sns-service/data/redis/src/main/kotlin/com/hyoseok/config/RedisKeys.kt
@@ -5,7 +5,7 @@ object RedisKeys {
     const val PRODUCT_ZSET_KEY = "product:keys"
     const val SNS_ZSET_KEY = "sns:keys"
 
-    fun getMemberKey(id: Long) = "{member}:$id"
-    fun getProductKey(id: Long) = "{product}:$id"
+    fun getMemberKey(id: Long) = "member:$id"
+    fun getProductKey(id: Long) = "product:$id"
     fun getSnsKey(id: Long) = "sns:$id"
 }
