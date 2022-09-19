d5584a8332a88bf08a082c45bc9aacaf933cfcbf
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-19 17:43:54 +0900

    chore: application:api 모듈 생성

---
+++ sns-feed-service/application/api/command/build.gradle.kts
@@ -0,0 +1,13 @@
+apply(plugin = "org.springframework.boot")
+
+dependencies {
+    implementation(project(":sns-service:common"))
+    implementation(project(":sns-service:data:jpa"))
+    implementation(project(":sns-service:data:redis"))
+    implementation(project(":sns-service:domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework:spring-tx")
+}
