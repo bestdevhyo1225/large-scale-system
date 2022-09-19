34ccefa3bb3e69dbfe046b9a331274e378538472
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-19 22:11:21 +0900

    chore: application 모듈 정리

--- sns-feed-service/application/event-worker/feed/build.gradle.kts
+++ sns-feed-service/application/event-worker/feed/build.gradle.kts
@@ -2,4 +2,6 @@ apply(plugin = "org.springframework.boot")
 
 dependencies {
     implementation(project(":sns-feed-service:infrastructure:kafka"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
