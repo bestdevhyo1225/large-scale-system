2a0bcc9aebd877df322bb3328e25ecfe4ad7ccc6
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-21 20:52:17 +0900

    style: 이름 변경

--- sns-feed-service/application/event-worker/feed-server/src/main/kotlin/com/hyoseok/SnsFeedEventWorker.kt
+++ sns-feed-service/application/event-worker/feed-server/src/main/kotlin/com/hyoseok/SnsFeedEventWorker.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FeedEventWorker
+class SnsFeedEventWorker
 
 fun main(args: Array<String>) {
-    runApplication<FeedEventWorker>(*args)
+    runApplication<SnsFeedEventWorker>(*args)
 }
