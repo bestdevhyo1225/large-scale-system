784425ed406966fb96cb2c104336b83381fda56b
janghyoseog <bestdevhyo1225@gmail.com>
2022-09-21 23:30:54 +0900

    feat: Feed 캐시에 만료 시간 설정

--- sns-feed-service/data/redis/feed/src/test/kotlin/com/hyoseok/repository/FeedCacheRepositoryTests.kt
+++ sns-feed-service/data/redis/feed/src/test/kotlin/com/hyoseok/repository/FeedCacheRepositoryTests.kt
@@ -1,10 +1,10 @@
 package com.hyoseok.repository
 
+import com.hyoseok.config.RedisFeedConfig
 import com.hyoseok.config.RedisFeedEmbbededServerConfig
 import com.hyoseok.config.RedisFeedServerProperties
-import com.hyoseok.config.RedisKeys
-import com.hyoseok.config.RedisFeedConfig
 import com.hyoseok.config.RedisFeedTemplateConfig
+import com.hyoseok.config.RedisKeys
 import com.hyoseok.feed.entity.FeedCache
 import com.hyoseok.feed.repository.FeedCacheReadRepository
 import com.hyoseok.feed.repository.FeedCacheRepository
@@ -13,7 +13,9 @@ import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.ints.shouldBeZero
+import io.kotest.matchers.longs.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
@@ -21,6 +23,7 @@ import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 @DataRedisTest
 @DirtiesContext
@@ -67,6 +70,37 @@ internal class FeedCacheRepositoryTests : DescribeSpec() {
 
                 feedCaches.size.shouldBe(1)
                 feedCaches.first().shouldBe(value)
+            }
+
+            context("이미 ExpireTime 값이 지정되어 있는 경우에는") {
+                it("데이터만 저장한다") {
+                    // given
+                    val memberId = 19283L
+                    val key: String = RedisKeys.getMemberFeedKey(id = memberId)
+                    val postIds: List<Long> = listOf(1245L, 1231L)
+                    val values: List<FeedCache> = postIds.map { FeedCache(postId = it) }
+                    val now: LocalDateTime = LocalDateTime.now().withNano(0)
+                    val scores: List<Double> = listOf(
+                        Timestamp.valueOf(now).time.toDouble(),
+                        Timestamp.valueOf(now.plusMinutes(5)).time.toDouble(),
+                    )
+
+                    feedCacheRepository.zadd(key = key, value = values.first(), score = scores.first())
+                    val firstExpireTime: Long = feedCacheReadRepository.getExpire(key = key, timeUnit = MILLISECONDS)
+
+                    delay(timeMillis = 300)
+
+                    // when
+                    feedCacheRepository.zadd(key = key, value = values[1], score = scores[1])
+                    val secondExpireTime: Long = feedCacheReadRepository.getExpire(key = key, timeUnit = MILLISECONDS)
+
+                    // then
+                    val feedCaches: List<FeedCache> =
+                        feedCacheReadRepository.zrevrange(key = key, start = 0, end = 1, clazz = FeedCache::class.java)
+
+                    feedCaches.size.shouldBe(2)
+                    firstExpireTime.shouldBeGreaterThan(secondExpireTime)
+                }
             }
         }
 
--- sns-feed-service/domain/src/main/kotlin/com/hyoseok/feed/repository/FeedCacheReadRepository.kt
+++ sns-feed-service/domain/src/main/kotlin/com/hyoseok/feed/repository/FeedCacheReadRepository.kt
@@ -1,5 +1,8 @@
 package com.hyoseok.feed.repository
 
+import java.util.concurrent.TimeUnit
+
 interface FeedCacheReadRepository {
+    fun getExpire(key: String, timeUnit: TimeUnit): Long
     fun <T> zrevrange(key: String, start: Long, end: Long, clazz: Class<T>): List<T>
 }
