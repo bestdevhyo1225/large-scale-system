30bd3e6857200d300b77a3828033afce044bb551
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-08 23:23:53 +0900

    test: 회원 통합 테스트 코드 추가
    [#91]

---
+++ coupon-service/application/coupon-command-api/src/test/kotlin/com/hyoseok/member/integration/MemberIntegrationTests.kt
@@ -0,0 +1,61 @@
+package com.hyoseok.member.integration
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.hyoseok.CouponCommandApiApplication
+import com.hyoseok.coupon.config.RedisCouponEmbbededServerConfig
+import com.hyoseok.member.controller.request.MemberCreateRequest
+import io.kotest.core.extensions.Extension
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest(classes = [CouponCommandApiApplication::class, RedisCouponEmbbededServerConfig::class])
+@DirtiesContext
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class MemberIntegrationTests : DescribeSpec() {
+
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    private val jacksonObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+
+    init {
+        this.describe("POST /members") {
+            context("회원을 생성하고") {
+                it("201 Created 응답을 반환한다") {
+                    // given
+                    val request = MemberCreateRequest(name = "Jang")
+
+                    // when
+                    val resultActions: ResultActions = mockMvc.perform(
+                        post("/members")
+                            .contentType(APPLICATION_JSON_VALUE)
+                            .content(jacksonObjectMapper.writeValueAsString(request)),
+                    ).andDo(print())
+
+                    // then
+                    resultActions
+                        .andExpect(status().isCreated)
+                        .andExpect(jsonPath("$.status").value("success"))
+                }
+            }
+        }
+    }
+}
