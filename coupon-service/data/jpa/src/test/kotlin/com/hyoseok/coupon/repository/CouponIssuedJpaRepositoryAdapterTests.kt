61d7ce63a8b607ae233ebbbee8b86f861b680897
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-09 17:08:28 +0900

    test: CouponIssuedReadRepository 단위 테스트 코드 추가
    [#91]

--- coupon-service/data/jpa/src/test/kotlin/com/hyoseok/coupon/repository/CouponIssuedJpaRepositoryAdapterTests.kt
+++ coupon-service/data/jpa/src/test/kotlin/com/hyoseok/coupon/repository/CouponIssuedJpaRepositoryAdapterTests.kt
@@ -6,8 +6,11 @@ import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.longs.shouldNotBeZero
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import org.springframework.beans.factory.annotation.Autowired
 
 internal class CouponIssuedJpaRepositoryAdapterTests : JpaRepositoryAdapterTests, DescribeSpec() {
@@ -17,6 +20,9 @@ internal class CouponIssuedJpaRepositoryAdapterTests : JpaRepositoryAdapterTests
 
     @Autowired
     private lateinit var couponIssuedRepository: CouponIssuedRepository
+
+    @Autowired
+    private lateinit var couponIssuedReadRepository: CouponIssuedReadRepository
 
     init {
         this.describe("save 메서드는") {
@@ -32,6 +38,48 @@ internal class CouponIssuedJpaRepositoryAdapterTests : JpaRepositoryAdapterTests
                 // then
                 couponIssued.id.shouldNotBeNull()
                 couponIssued.id.shouldNotBeZero()
+            }
+        }
+
+        this.describe("findByCouponIdAndMemberId 메서드는") {
+            it("회원이 발급 받은 쿠폰 발급 데이터를 반환한다") {
+                // given
+                val couponId = 1L
+                val memberId = 1L
+                val couponIssued = CouponIssued(couponId = couponId, memberId = memberId)
+
+                couponIssuedRepository.save(couponIssued = couponIssued)
+
+                // when
+                val findCouponIssued: CouponIssued =
+                    couponIssuedReadRepository.findByCouponIdAndMemberId(couponId = couponId, memberId = memberId)
+
+                // then
+                findCouponIssued.shouldBe(couponIssued)
+            }
+        }
+
+        this.describe("findByMemberIdAndlimitAndOffset 메서드는") {
+            it("회원이 발급받은 쿠폰 발급 리스트를 반환한다") {
+                // given
+                val couponId = 1L
+                val memberId = 1L
+                val couponIssued = CouponIssued(couponId = couponId, memberId = memberId)
+
+                couponIssuedRepository.save(couponIssued = couponIssued)
+
+                // when
+                val result: Pair<Long, List<CouponIssued>> =
+                    couponIssuedReadRepository.findByMemberIdAndlimitAndOffset(
+                        memberId = memberId,
+                        limit = 10,
+                        offset = 0,
+                    )
+
+                result.first.shouldNotBeZero()
+                result.first.shouldBe(1)
+                result.second.shouldNotBeEmpty()
+                result.second.shouldHaveSize(1)
             }
         }
     }
