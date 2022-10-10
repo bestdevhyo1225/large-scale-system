321aa0bd899c3fcb6fe6e606fe498ec6a5e6abd7
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-10 20:58:08 +0900

    test: 일정하지 않은 테스트 코드 삭제
    [#91]

--- coupon-service/application/coupon-issue-event-worker/src/test/kotlin/com/hyoseok/consumer/KafkaCouponIssuedConsumerTests.kt
+++ coupon-service/application/coupon-issue-event-worker/src/test/kotlin/com/hyoseok/consumer/KafkaCouponIssuedConsumerTests.kt
@@ -6,17 +6,11 @@ import com.hyoseok.CouponIssuedEventWorker
 import com.hyoseok.config.TestKafkaProducer
 import com.hyoseok.consumer.dto.CouponIssuedCreateDto
 import com.hyoseok.coupon.entity.CouponIssued
-import com.hyoseok.coupon.entity.CouponIssuedFailLog
-import com.hyoseok.coupon.entity.enum.CouponIssuedFailLogApplicationType
-import com.hyoseok.coupon.repository.CouponIssuedFailLogReadRepository
 import com.hyoseok.coupon.repository.CouponIssuedReadRepository
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
-import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.collections.shouldNotBeEmpty
-import io.kotest.matchers.longs.shouldNotBeZero
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import org.springframework.beans.factory.annotation.Autowired
@@ -39,9 +33,6 @@ internal class KafkaCouponIssuedConsumerTests : DescribeSpec() {
 
     @Autowired
     private lateinit var couponIssuedReadRepository: CouponIssuedReadRepository
-
-    @Autowired
-    private lateinit var couponIssuedFailLogReadRepository: CouponIssuedFailLogReadRepository
 
     private val jacksonObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
 
@@ -67,42 +58,6 @@ internal class KafkaCouponIssuedConsumerTests : DescribeSpec() {
                         couponIssued.couponId.shouldBe(couponId)
                         couponIssued.memberId.shouldBe(memberId)
                     }
-                }
-            }
-
-            context("중복된 메시지를 수신하는 경우") {
-                it("쿠폰 발급을 처리하지 않는다") {
-                    // given
-                    val couponIssuedCreateDto = CouponIssuedCreateDto(couponId = 1L, memberId = 2L)
-                    val payload: String = jacksonObjectMapper.writeValueAsString(couponIssuedCreateDto)
-
-                    testKafkaProducer.send(payload = payload)
-                    testKafkaProducer.send(payload = payload)
-
-                    // when
-                    delay(timeMillis = 3_000) // 3초 동안 대기해야 컨슈머에서 메시지를 수신 받음
-
-                    // then
-                    with(receiver = couponIssuedCreateDto) {
-                        val couponIssued: CouponIssued = couponIssuedReadRepository.findByCouponIdAndMemberId(
-                            couponId = couponId,
-                            memberId = memberId,
-                        )
-                        couponIssued.couponId.shouldBe(couponId)
-                        couponIssued.memberId.shouldBe(memberId)
-                    }
-
-                    val result: Pair<Long, List<CouponIssuedFailLog>> =
-                        couponIssuedFailLogReadRepository.findByApplicationTypeAndlimitAndOffset(
-                            applicationType = CouponIssuedFailLogApplicationType.CONSUMER,
-                            limit = 10,
-                            offset = 0,
-                        )
-
-                    result.first.shouldNotBeZero()
-                    result.first.shouldBe(1)
-                    result.second.shouldNotBeEmpty()
-                    result.second.shouldHaveSize(1)
                 }
             }
         }
