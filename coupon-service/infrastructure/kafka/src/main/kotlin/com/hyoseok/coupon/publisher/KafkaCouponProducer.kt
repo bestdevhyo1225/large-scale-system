6b67be34792026679855dc16b9eec07f03fe77c7
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-19 23:12:30 +0900

    refactor: KafkaCouponProducerCallback 클래스를 주입하도록 리팩토링
    [#91]

--- coupon-service/infrastructure/kafka/src/main/kotlin/com/hyoseok/coupon/publisher/KafkaCouponProducer.kt
+++ coupon-service/infrastructure/kafka/src/main/kotlin/com/hyoseok/coupon/publisher/KafkaCouponProducer.kt
@@ -20,6 +20,7 @@ class KafkaCouponProducer(
     @Value("\${infrastructure.kafka.topics.coupon-issued}")
     private val topic: String,
     private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val kafkaCouponProducerCallback: KafkaCouponProducerCallback,
 ) : CouponMessageBrokerProducer {
 
     private val logger = KotlinLogging.logger {}
@@ -41,7 +42,7 @@ class KafkaCouponProducer(
         execute {
             kafkaTemplate
                 .send(topic, jacksonObjectMapper.writeValueAsString(event))
-                .addCallback(KafkaCouponProducerCallback())
+                .addCallback(kafkaCouponProducerCallback)
         }
     }
 
