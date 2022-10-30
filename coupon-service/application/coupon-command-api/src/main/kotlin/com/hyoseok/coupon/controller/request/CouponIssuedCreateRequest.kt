c11b39cd26695444cc8269dffd24d2a07c14d390
janghyoseog <bestdevhyo1225@gmail.com>
2022-10-30 13:53:00 +0900

    feat: @field:Schema 를 활용한 example value 초기화 처리

--- coupon-service/application/coupon-command-api/src/main/kotlin/com/hyoseok/coupon/controller/request/CouponIssuedCreateRequest.kt
+++ coupon-service/application/coupon-command-api/src/main/kotlin/com/hyoseok/coupon/controller/request/CouponIssuedCreateRequest.kt
@@ -1,10 +1,12 @@
 package com.hyoseok.coupon.controller.request
 
 import com.hyoseok.coupon.service.dto.CouponIssuedCreateDto
+import io.swagger.v3.oas.annotations.media.Schema
 import javax.validation.constraints.Positive
 
 data class CouponIssuedCreateRequest(
     @field:Positive(message = "memberId는 0보다 큰 값을 입력하세요")
+    @field:Schema(description = "회원 고유번호", example = "1", required = true)
     val memberId: Long,
 ) {
     fun toServiceDto(couponId: Long) = CouponIssuedCreateDto(memberId = memberId, couponId = couponId)
--- coupon-service/application/coupon-command-api/src/main/kotlin/com/hyoseok/member/controller/request/MemberCreateRequest.kt
+++ coupon-service/application/coupon-command-api/src/main/kotlin/com/hyoseok/member/controller/request/MemberCreateRequest.kt
@@ -1,10 +1,12 @@
 package com.hyoseok.member.controller.request
 
 import com.hyoseok.member.service.dto.MemberCreateDto
+import io.swagger.v3.oas.annotations.media.Schema
 import javax.validation.constraints.NotBlank
 
 data class MemberCreateRequest(
     @field:NotBlank(message = "name을 입력하세요")
+    @field:Schema(description = "회원 이름", example = "JangHyoSeok", required = true)
     val name: String,
 ) {
     fun toServiceDto() = MemberCreateDto(name = name)
