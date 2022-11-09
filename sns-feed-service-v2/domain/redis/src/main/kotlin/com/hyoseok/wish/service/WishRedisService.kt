b9ea79819df239e07f310d4c0eb723bde87db5e3
janghyoseog <bestdevhyo1225@gmail.com>
2022-11-09 22:19:36 +0900

    refactor: domain:redis 모듈의 wish 도메인 리팩토링
    - #149

--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/wish/service/WishRedisService.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/wish/service/WishRedisService.kt
@@ -2,6 +2,7 @@ package com.hyoseok.wish.service
 
 import com.hyoseok.wish.dto.WishCacheDto
 import com.hyoseok.wish.repository.WishRedisTransactionRepository
+import com.hyoseok.wish.service.WishRedisService.ErrorMessage.FAIL_CRAETE_WISH
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
@@ -11,6 +12,13 @@ class WishRedisService(
     private val wishRedisTransactionRepository: WishRedisTransactionRepository,
 ) {
 
-    fun create(dto: WishCacheDto): Boolean =
-        wishRedisTransactionRepository.createWish(wishCache = dto.toEntity())
+    object ErrorMessage {
+        const val FAIL_CRAETE_WISH = "좋아요 캐시를 저장하는데 실패했습니다"
+    }
+
+    fun create(dto: WishCacheDto) {
+        if (!wishRedisTransactionRepository.createWish(wishCache = dto.toEntity())) {
+            throw RuntimeException(FAIL_CRAETE_WISH)
+        }
+    }
 }
