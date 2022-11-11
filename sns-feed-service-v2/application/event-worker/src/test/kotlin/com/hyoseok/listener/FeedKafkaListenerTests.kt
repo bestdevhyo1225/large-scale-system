b2b864c8e90a571b6b02defd0a5a9c6ef3a52a34
janghyoseog <bestdevhyo1225@gmail.com>
2022-11-12 00:23:26 +0900

    test: 테스트 주석 처리

--- sns-feed-service-v2/application/event-worker/src/test/kotlin/com/hyoseok/listener/FeedKafkaListenerTests.kt
+++ sns-feed-service-v2/application/event-worker/src/test/kotlin/com/hyoseok/listener/FeedKafkaListenerTests.kt
@@ -1,26 +1,26 @@
-//package com.hyoseok.listener
+// package com.hyoseok.listener
 //
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-//import com.hyoseok.SnsFeedEventWorkerApplication
-//import com.hyoseok.config.FeedKafkaProducer
-//import com.hyoseok.listener.dto.FeedEventDto
-//import io.kotest.core.extensions.Extension
-//import io.kotest.core.spec.IsolationMode
-//import io.kotest.core.spec.style.DescribeSpec
-//import io.kotest.extensions.spring.SpringExtension
-//import kotlinx.coroutines.delay
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.context.SpringBootTest
-//import org.springframework.kafka.test.context.EmbeddedKafka
+// import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+// import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+// import com.hyoseok.SnsFeedEventWorkerApplication
+// import com.hyoseok.config.FeedKafkaProducer
+// import com.hyoseok.listener.dto.FeedEventDto
+// import io.kotest.core.extensions.Extension
+// import io.kotest.core.spec.IsolationMode
+// import io.kotest.core.spec.style.DescribeSpec
+// import io.kotest.extensions.spring.SpringExtension
+// import kotlinx.coroutines.delay
+// import org.springframework.beans.factory.annotation.Autowired
+// import org.springframework.boot.test.context.SpringBootTest
+// import org.springframework.kafka.test.context.EmbeddedKafka
 //
-//@SpringBootTest(classes = [SnsFeedEventWorkerApplication::class, FeedKafkaProducer::class])
-//@EmbeddedKafka(
+// @SpringBootTest(classes = [SnsFeedEventWorkerApplication::class, FeedKafkaProducer::class])
+// @EmbeddedKafka(
 //    brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"],
 //    topics = ["feed-topic"],
 //    partitions = 1,
-//)
-//internal class FeedKafkaListenerTests : DescribeSpec() {
+// )
+// internal class FeedKafkaListenerTests : DescribeSpec() {
 //
 //    override fun extensions(): List<Extension> = listOf(SpringExtension)
 //    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
@@ -48,4 +48,4 @@
 //            }
 //        }
 //    }
-//}
+// }
