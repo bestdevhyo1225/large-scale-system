3d13dcf0d23e8d1444db321190b37f895e8fe0a8
janghyoseog <bestdevhyo1225@gmail.com>
2022-11-16 21:35:43 +0900

    feat: redis connection pool 옵션 추가

--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/config/FeedRedisConfig.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/config/FeedRedisConfig.kt
@@ -4,6 +4,7 @@ import com.hyoseok.config.RedisMode.Cluster
 import com.hyoseok.config.RedisMode.Standalone
 import io.lettuce.core.ReadFrom
 import mu.KotlinLogging
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cache.annotation.EnableCaching
@@ -12,8 +13,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisClusterConfiguration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration
+import java.time.Duration
 
 @Configuration
 @EnableCaching(proxyTargetClass = true)
@@ -21,8 +23,21 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 class FeedRedisConfig(
     @Value("\${spring.feed.redis.mode}")
     private val mode: RedisMode,
+
     @Value("\${spring.feed.redis.nodes}")
     private val nodes: List<String>,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-active}")
+    private val maxActive: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-idle}")
+    private val maxIdle: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-idle}")
+    private val minIdle: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-wait}")
+    private val maxWait: Long,
 ) {
 
     private val logger = KotlinLogging.logger {}
@@ -34,13 +49,13 @@ class FeedRedisConfig(
                 logger.info { "feed redis standalone mode" }
 
                 val (host: String, port: Int) = getHostAndPort()
-                LettuceConnectionFactory(RedisStandaloneConfiguration(host, port), lettuceClientConfig())
+                LettuceConnectionFactory(RedisStandaloneConfiguration(host, port), lettucePoolingClientConfiguration())
             }
 
             Cluster -> {
                 logger.info { "feed redis cluster mode" }
 
-                LettuceConnectionFactory(RedisClusterConfiguration(nodes), lettuceClientConfig())
+                LettuceConnectionFactory(RedisClusterConfiguration(nodes), lettucePoolingClientConfiguration())
             }
         }
 
@@ -50,11 +65,26 @@ class FeedRedisConfig(
         }
     }
 
-    private fun lettuceClientConfig(): LettuceClientConfiguration =
-        LettuceClientConfiguration.builder()
+    /*
+    * [ Redis Connection Pool을 사용하는 이유 ]
+    * - Redis의 multi 명령어를 사용하면 트랜잭션을 사용할 수 있다.
+    * - 트랜잭션은 새로운 커넥션을 매 번 생성하고 종료한다. -> 비용 문제
+    * - 위와 같은 이유로 커넥션 풀을 사용하려고 한다.
+    * - 트랜잭션을 제외한 나머지 명령어의 경우, shareNativeConnection=true(default) 에 의해 하나의 공유된 커넥션을 사용한다.
+    * */
+    private fun lettucePoolingClientConfiguration(): LettucePoolingClientConfiguration {
+        val poolConfig = GenericObjectPoolConfig<Any>()
+        poolConfig.maxTotal = maxActive
+        poolConfig.maxIdle = maxIdle
+        poolConfig.minIdle = minIdle
+        poolConfig.setMaxWait(Duration.ofMillis(maxWait))
+
+        return LettucePoolingClientConfiguration.builder()
             .clientName("feed-redis-client")
             .readFrom(ReadFrom.REPLICA_PREFERRED)
+            .poolConfig(poolConfig)
             .build()
+    }
 
     private fun getHostAndPort(): Pair<String, Int> {
         val splits: List<String> = nodes.first().split(":")
--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/config/PostRedisConfig.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/config/PostRedisConfig.kt
@@ -4,6 +4,7 @@ import com.hyoseok.config.RedisMode.Cluster
 import com.hyoseok.config.RedisMode.Standalone
 import io.lettuce.core.ReadFrom
 import mu.KotlinLogging
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cache.annotation.EnableCaching
@@ -13,8 +14,9 @@ import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.connection.RedisClusterConfiguration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration
+import java.time.Duration
 
 @Configuration
 @EnableCaching(proxyTargetClass = true)
@@ -22,8 +24,21 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 class PostRedisConfig(
     @Value("\${spring.post.redis.mode}")
     private val mode: RedisMode,
+
     @Value("\${spring.post.redis.nodes}")
     private val nodes: List<String>,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-active}")
+    private val maxActive: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-idle}")
+    private val maxIdle: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-idle}")
+    private val minIdle: Int,
+
+    @Value("\${spring.feed.redis.lettuce.pool.max-wait}")
+    private val maxWait: Long,
 ) {
 
     private val logger = KotlinLogging.logger {}
@@ -36,16 +51,13 @@ class PostRedisConfig(
                 logger.info { "post redis standalone mode" }
 
                 val (host: String, port: Int) = getHostAndPort()
-                LettuceConnectionFactory(RedisStandaloneConfiguration(host, port), lettuceClientConfig())
+                LettuceConnectionFactory(RedisStandaloneConfiguration(host, port), lettucePoolingClientConfiguration())
             }
 
             Cluster -> {
                 logger.info { "post redis cluster mode" }
 
-                LettuceConnectionFactory(
-                    RedisClusterConfiguration(nodes),
-                    lettuceClientConfig(),
-                )
+                LettuceConnectionFactory(RedisClusterConfiguration(nodes), lettucePoolingClientConfiguration())
             }
         }
 
@@ -55,11 +67,26 @@ class PostRedisConfig(
         }
     }
 
-    private fun lettuceClientConfig(): LettuceClientConfiguration =
-        LettuceClientConfiguration.builder()
+    /*
+    * [ Redis Connection Pool을 사용하는 이유 ]
+    * - Redis의 multi 명령어를 사용하면 트랜잭션을 사용할 수 있다.
+    * - 트랜잭션은 새로운 커넥션을 매 번 생성하고 종료한다. -> 비용 문제
+    * - 위와 같은 이유로 커넥션 풀을 사용하려고 한다.
+    * - 트랜잭션을 제외한 나머지 명령어의 경우, shareNativeConnection=true(default) 에 의해 하나의 공유된 커넥션을 사용한다.
+    * */
+    private fun lettucePoolingClientConfiguration(): LettucePoolingClientConfiguration {
+        val poolConfig = GenericObjectPoolConfig<Any>()
+        poolConfig.maxTotal = maxActive
+        poolConfig.maxIdle = maxIdle
+        poolConfig.minIdle = minIdle
+        poolConfig.setMaxWait(Duration.ofMillis(maxWait))
+
+        return LettucePoolingClientConfiguration.builder()
             .clientName("post-redis-client")
             .readFrom(ReadFrom.REPLICA_PREFERRED)
+            .poolConfig(poolConfig)
             .build()
+    }
 
     private fun getHostAndPort(): Pair<String, Int> {
         val splits: List<String> = nodes.first().split(":")
