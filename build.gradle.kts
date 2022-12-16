71251b9f08c4c14f0fa40763ad0d3e7476e915eb
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-17 00:02:43 +0900

    chore: ktlint 0.47.0 버전으로 고정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin
 plugins {
     id("org.springframework.boot") version "2.7.2" apply false
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
