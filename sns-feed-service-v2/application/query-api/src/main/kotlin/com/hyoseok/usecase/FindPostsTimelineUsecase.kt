89895846cd814f553d7eba0955f94805530b309e
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-17 23:38:09 +0900

    refactor: FindPostTimelineUsecase 리팩토링

--- sns-feed-service-v2/application/query-api/src/main/kotlin/com/hyoseok/usecase/FindPostsTimelineUsecase.kt
+++ sns-feed-service-v2/application/query-api/src/main/kotlin/com/hyoseok/usecase/FindPostsTimelineUsecase.kt
@@ -4,10 +4,7 @@ import com.hyoseok.config.resilience4j.ratelimiter.RateLimiterConfig.Name.FIND_P
 import com.hyoseok.exception.QueryApiRateLimitException
 import com.hyoseok.feed.dto.FeedDto
 import com.hyoseok.feed.service.FeedRedisReadService
-import com.hyoseok.follow.service.FollowReadService
 import com.hyoseok.mapper.PostDtoMapper
-import com.hyoseok.member.dto.MemberDto
-import com.hyoseok.member.service.MemberReadService
 import com.hyoseok.post.dto.PostCacheDto
 import com.hyoseok.post.dto.PostDto
 import com.hyoseok.post.service.PostReadService
@@ -15,18 +12,12 @@ import com.hyoseok.post.service.PostRedisReadService
 import com.hyoseok.util.PageByPosition
 import com.hyoseok.util.PageRequestByPosition
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 
 @Service
 class FindPostsTimelineUsecase(
-    private val followReadService: FollowReadService,
     private val feedRedisReadService: FeedRedisReadService,
-    private val memberReadService: MemberReadService,
     private val postRedisReadService: PostRedisReadService,
     private val postReadService: PostReadService,
 ) {
@@ -34,48 +25,23 @@ class FindPostsTimelineUsecase(
     private val logger = KotlinLogging.logger {}
 
     @RateLimiter(name = FIND_POST_TIMELINE_USECASE, fallbackMethod = "fallbackExecute")
-    fun execute(memberId: Long, pageRequestByPosition: PageRequestByPosition): PageByPosition<PostDto> = runBlocking {
-        val (
-            feedPostsRequestByPosition: PageRequestByPosition,
-            influencerPostsRequestByPosition: PageRequestByPosition,
-        ) = pageRequestByPosition.splitTwoPageRequestByPosition()
-
-        val deferredFeedPosts: Deferred<List<PostDto>> = async(context = Dispatchers.IO) {
-            findFeedPosts(memberId = memberId, pageRequestByPosition = feedPostsRequestByPosition)
-        }
-        val deferredInfluencerPosts: Deferred<List<PostDto>> = async(context = Dispatchers.IO) {
-            findInfluencerPosts(memberId = memberId, pageRequestByPosition = influencerPostsRequestByPosition)
-        }
-        val postDtos: List<PostDto> = deferredFeedPosts.await().plus(deferredInfluencerPosts.await())
-
-        PageByPosition(
-            items = postDtos.shuffled(),
-            nextPageRequestByPosition = PageRequestByPosition(
-                start = feedPostsRequestByPosition.start.plus(feedPostsRequestByPosition.size),
-                size = feedPostsRequestByPosition.size,
-            ),
-        )
-    }
-
-    private suspend fun findFeedPosts(
-        memberId: Long,
-        pageRequestByPosition: PageRequestByPosition,
-    ): List<PostDto> {
+    fun execute(memberId: Long, pageRequestByPosition: PageRequestByPosition): PageByPosition<PostDto> {
         val feedDtos: List<FeedDto> =
             feedRedisReadService.findFeeds(memberId = memberId, pageRequestByPosition = pageRequestByPosition)
         val postIds: List<Long> = feedDtos.map { it.postId }
         val postCacheDtos: List<PostCacheDto> = postRedisReadService.findPostCaches(ids = postIds)
-        return postCacheDtos.map { PostDtoMapper.of(postCacheDto = it) }
-    }
 
-    private suspend fun findInfluencerPosts(
-        memberId: Long,
-        pageRequestByPosition: PageRequestByPosition,
-    ): List<PostDto> {
-        val followeeIds: List<Long> = followReadService.findFolloweeIdsByStaticLimit(followerId = memberId)
-        val memberDtos: List<MemberDto> = memberReadService.findInfluencerMembers(ids = followeeIds)
-        val memberIds: List<Long> = memberDtos.map { it.id }
-        return postReadService.findPosts(memberIds = memberIds, pageRequestByPosition = pageRequestByPosition)
+        val postDtos: List<PostDto> =
+            if (postCacheDtos.isNotEmpty()) {
+                postCacheDtos.map { PostDtoMapper.of(postCacheDto = it) }
+            } else {
+                postReadService.findPosts(ids = postIds)
+            }
+
+        return PageByPosition(
+            items = postDtos,
+            nextPageRequestByPosition = pageRequestByPosition.next(itemSize = postDtos.size),
+        )
     }
 
     private fun fallbackExecute(exception: Exception): PageByPosition<PostDto> {
