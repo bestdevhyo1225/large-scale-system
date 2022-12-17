2b5db1c3273c9ac1ba6013ee5467ca4e0f8d84f1
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-18 02:29:24 +0900

    refactor: Member 도메인 리팩토링

--- sns-feed-service-v2/domain/rds/src/main/kotlin/com/hyoseok/member/entity/Member.kt
+++ sns-feed-service-v2/domain/rds/src/main/kotlin/com/hyoseok/member/entity/Member.kt
@@ -13,7 +13,7 @@ import javax.persistence.Table
 @DynamicUpdate
 class Member private constructor(
     name: String,
-    influencer: Boolean,
+    influencer: Int,
     createdAt: LocalDateTime,
     updatedAt: LocalDateTime,
 ) : BaseEntity(createdAt = createdAt) {
@@ -23,7 +23,7 @@ class Member private constructor(
         protected set
 
     @Column(name = "influencer", nullable = false)
-    var influencer: Boolean = influencer
+    var influencer: Int = influencer
         protected set
 
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
@@ -35,17 +35,17 @@ class Member private constructor(
 
     object ErrorMessage {
         const val MAX_LIMIT = "입력할 수 있는 이름의 길이를 초과했습니다"
-        const val NOT_INFLUENCER = "인플루언서가 기준에 충족하지 못하기 때문에 인플루언서 계정으로 변경할 수 없습니다"
-        const val NOT_NORMAL = "인플루언서 회원 기준에 충족하기 때문에 일반 계정으로 변경할 수 없습니다"
     }
 
     companion object {
         private const val MAX_LIMIT_NAME_LENGTH = 20
+        const val INFLUENCER_FALSE = 0
+        const val INFLUENCER_TRUE = 1
 
         operator fun invoke(name: String): Member {
             validateName(name = name)
             val nowDatetime: LocalDateTime = LocalDateTime.now()
-            return Member(name = name, influencer = false, createdAt = nowDatetime, updatedAt = nowDatetime)
+            return Member(name = name, influencer = INFLUENCER_FALSE, createdAt = nowDatetime, updatedAt = nowDatetime)
         }
 
         private fun validateName(name: String) {
@@ -55,9 +55,9 @@ class Member private constructor(
         }
     }
 
-    fun isInfluencer(): Boolean = influencer
+    fun getInfluencer(): Boolean = influencer == INFLUENCER_TRUE
 
-    public fun changeInfluencer(influencer: Boolean) {
-        this.influencer = influencer
+    fun changeInfluencer(influencer: Boolean) {
+        this.influencer = if (influencer) INFLUENCER_TRUE else INFLUENCER_FALSE
     }
 }
