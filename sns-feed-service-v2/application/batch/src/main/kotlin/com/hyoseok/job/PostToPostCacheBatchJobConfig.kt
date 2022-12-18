9b3cfbcd4a04657e1c159899bef5ba76791aaafb
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-18 22:38:21 +0900

    feat: PostToPostCacheBatchJobConfig 클래스 추가

---
+++ sns-feed-service-v2/application/batch/src/main/kotlin/com/hyoseok/job/PostToPostCacheBatchJobConfig.kt
@@ -0,0 +1,87 @@
+package com.hyoseok.job
+
+import com.hyoseok.post.dto.PostCacheDto
+import com.hyoseok.post.dto.PostImageCacheDto
+import com.hyoseok.post.entity.Post
+import com.hyoseok.post.service.PostRedisService
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.database.JpaPagingItemReader
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManagerFactory
+
+@Configuration
+class PostToPostCacheBatchJobConfig(
+    @Value("\${spring.batch.chunk-size:1000}")
+    private val chunkSize: Int,
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory,
+    private val entityManagerFactory: EntityManagerFactory,
+    private val postRedisService: PostRedisService,
+) {
+
+    companion object {
+        const val POST_TO_POST_CACHE = "PostToPostCache"
+    }
+
+    @Bean(name = ["${POST_TO_POST_CACHE}Job"])
+    fun job(): Job =
+        jobBuilderFactory.get("${POST_TO_POST_CACHE}Job")
+            .preventRestart()
+            .start(step())
+            .build()
+
+    @Bean(name = ["${POST_TO_POST_CACHE}Step"])
+    fun step(): Step =
+        stepBuilderFactory.get("${POST_TO_POST_CACHE}Step")
+            .chunk<Post, PostCacheDto>(chunkSize)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer())
+            .build()
+
+    @Bean(name = ["${POST_TO_POST_CACHE}Reader"])
+    fun reader(): JpaPagingItemReader<Post> =
+        JpaPagingItemReaderBuilder<Post>()
+            .name("${POST_TO_POST_CACHE}Reader")
+            .entityManagerFactory(entityManagerFactory)
+            .pageSize(chunkSize)
+            .queryString("SELECT p FROM Post p INNER JOIN FETCH p.postImages WHERE p.deletedAt IS NULL")
+            .build()
+
+    @Bean(name = ["${POST_TO_POST_CACHE}Processor"])
+    fun processor(): ItemProcessor<Post, PostCacheDto> =
+        ItemProcessor { post ->
+            with(receiver = post) {
+                PostCacheDto(
+                    id = id!!,
+                    memberId = memberId,
+                    title = title,
+                    contents = contents,
+                    writer = writer,
+                    viewCount = viewCount,
+                    wishCount = wishCount,
+                    createdAt = createdAt,
+                    updatedAt = updatedAt,
+                    images = postImages.map {
+                        PostImageCacheDto(id = it.id!!, url = it.url, sortOrder = it.sortOrder)
+                    },
+                )
+            }
+        }
+
+    @Bean(name = ["${POST_TO_POST_CACHE}Writer"])
+    fun writer(): ItemWriter<PostCacheDto> =
+        ItemWriter { postCacheDtos ->
+            postCacheDtos.forEach { postCacheDto ->
+                postRedisService.createOrUpdate(dto = postCacheDto)
+            }
+        }
+}
---
+++ sns-feed-service-v2/application/batch/src/test/kotlin/com/hyoseok/job/PostToPostCacheBatchJobConfigTests.kt
@@ -0,0 +1,111 @@
+package com.hyoseok.job
+
+import com.hyoseok.config.BasicDataSourceConfig
+import com.hyoseok.config.BatchConfig
+import com.hyoseok.config.PostRedisConfig
+import com.hyoseok.config.PostRedisTemplateConfig
+import com.hyoseok.config.jpa.JpaConfig
+import com.hyoseok.post.entity.Post
+import com.hyoseok.post.entity.PostCache
+import com.hyoseok.post.entity.PostImage
+import com.hyoseok.post.repository.PostRedisRepository
+import com.hyoseok.post.repository.PostRedisRepositoryImpl
+import com.hyoseok.post.repository.PostRedisTransactionRepositoryImpl
+import com.hyoseok.post.repository.PostRepository
+import com.hyoseok.post.service.PostRedisService
+import io.kotest.core.extensions.Extension
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.shouldBe
+import org.springframework.batch.core.BatchStatus
+import org.springframework.batch.core.ExitStatus
+import org.springframework.batch.core.JobExecution
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
+
+@SpringBatchTest
+@SpringBootTest(
+    classes = [
+        BatchConfig::class,
+        BasicDataSourceConfig::class,
+        JpaConfig::class,
+        PostRedisConfig::class,
+        PostRedisTemplateConfig::class,
+        PostToPostCacheBatchJobConfig::class,
+        PostRedisTransactionRepositoryImpl::class,
+        PostRedisRepositoryImpl::class,
+        PostRedisService::class,
+        PostRepository::class,
+    ],
+)
+internal class PostToPostCacheBatchJobConfigTests : DescribeSpec() {
+
+    override fun extensions(): List<Extension> = listOf(SpringExtension)
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
+
+    @Autowired
+    private lateinit var jobLauncherTestUtils: JobLauncherTestUtils
+
+    @Autowired
+    @Qualifier("postRedisTemplate")
+    private lateinit var redisTemplate: RedisTemplate<String, String?>
+
+    @Autowired
+    private lateinit var postRepository: PostRepository
+
+    @Autowired
+    private lateinit var postRedisRepository: PostRedisRepository
+
+    private val postIds: MutableList<Long> = mutableListOf()
+
+    init {
+        this.beforeSpec {
+            val title = "title"
+            val contents = "contents"
+            val writer = "writer"
+            (1L..10L).forEach {
+                val post = Post(
+                    memberId = it,
+                    title = title,
+                    contents = contents,
+                    writer = writer,
+                    postImages = listOf(
+                        PostImage(url = "test1", sortOrder = 1),
+                        PostImage(url = "test2", sortOrder = 2),
+                    ),
+                )
+                postRepository.save(post)
+                postIds.add(post.id!!)
+            }
+        }
+
+        this.afterSpec {
+            redisTemplate.delete(redisTemplate.keys("*"))
+        }
+
+        this.describe("PostToPostCacheBatchJob 클래스는") {
+            it("Post 엔티티를 PostCache 엔티티에 반영한다") {
+                // given
+                val keys: List<String> = postIds.map { PostCache.getPostIdKey(id = it) }
+
+                // when
+                val jobExecution: JobExecution = jobLauncherTestUtils.launchJob()
+
+                // then
+                jobExecution.status.shouldBe(BatchStatus.COMPLETED)
+                jobExecution.exitStatus.shouldBe(ExitStatus.COMPLETED)
+
+                postRedisRepository.mget(keys)
+                    .shouldNotBeEmpty()
+                    .shouldHaveSize(keys.size)
+            }
+        }
+    }
+}
