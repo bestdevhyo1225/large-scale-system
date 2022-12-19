e8413b52b7fdb00212da5950163565d2e0ba72d0
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-19 23:59:07 +0900

    refactor: Post Redis 도메인 리팩토링

--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/entity/PostCache.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/entity/PostCache.kt
@@ -18,12 +18,15 @@ data class PostCache(
         private const val HASH_MAX_ENTRIES = 1_000L
 
         const val POST_MEMBER_MAX_LIMIT = -10_001L
+        const val POST_CACHE_EXPIRE_TIME = 60L * 30L // 30분
+        const val POST_IDS_CACHE_BY_MEMBER_ID_EXPIRE_TIME = 60L * 60L * 24 * 30L // 30일
 
         fun getPostBucketKey(id: Long) = "post:bucket:${id.div(HASH_MAX_ENTRIES)}"
         fun getPostViewBucketKey(id: Long) = "post:view:bucket:${id.div(HASH_MAX_ENTRIES)}"
         fun getPostMemberIdBucketKey(memberId: Long) = "post:memberid:bucket:${memberId.div(HASH_MAX_ENTRIES)}"
         fun getPostIdsByMemberIdKey(memberId: Long) = "post:ids:member:$memberId"
         fun getPostIdKey(id: Long) = "post:$id"
-        fun getPostKeyAndExpireTime(id: Long) = Pair(first = getPostIdKey(id = id), second = 60L * 30L) // 30분
+        fun getPostKeyAndExpireTime(id: Long) =
+            Pair(first = getPostIdKey(id = id), second = POST_CACHE_EXPIRE_TIME)
     }
 }
--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/service/PostRedisService.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/service/PostRedisService.kt
@@ -1,17 +1,36 @@
 package com.hyoseok.post.service
 
 import com.hyoseok.post.dto.PostCacheDto
-import com.hyoseok.post.repository.PostRedisTransactionRepository
+import com.hyoseok.post.entity.PostCache
+import com.hyoseok.post.entity.PostCache.Companion.POST_CACHE_EXPIRE_TIME
+import com.hyoseok.post.entity.PostCache.Companion.POST_IDS_CACHE_BY_MEMBER_ID_EXPIRE_TIME
+import com.hyoseok.post.entity.PostCache.Companion.getPostIdKey
+import com.hyoseok.post.entity.PostCache.Companion.getPostIdsByMemberIdKey
+import com.hyoseok.post.repository.PostRedisRepository
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit.SECONDS
 
 @Service
 @ConditionalOnProperty(prefix = "spring.post.redis", name = ["enable"], havingValue = "true")
 class PostRedisService(
-    private val postRedisTransactionRepository: PostRedisTransactionRepository,
+    private val postRedisRepository: PostRedisRepository,
 ) {
 
     fun createOrUpdate(dto: PostCacheDto) {
-        postRedisTransactionRepository.createPostCache(postCache = dto.toEntity())
+        val postCache: PostCache = dto.toEntity()
+        postRedisRepository.set(
+            key = getPostIdKey(id = postCache.id),
+            value = postCache,
+            expireTime = POST_CACHE_EXPIRE_TIME,
+            timeUnit = SECONDS,
+        )
+        postRedisRepository.zaddAndExpire(
+            key = getPostIdsByMemberIdKey(memberId = postCache.memberId),
+            value = postCache.id,
+            score = postCache.id.toDouble(),
+            expireTime = POST_IDS_CACHE_BY_MEMBER_ID_EXPIRE_TIME,
+            timeUnit = SECONDS,
+        )
     }
 }
