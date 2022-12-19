bdb21b69995c5c8bdc804ceebb709837759678e6
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-20 00:10:10 +0900

    refactor: FindPostTimelineUsecase 리팩토링

--- sns-feed-service-v2/application/query-api/src/main/kotlin/com/hyoseok/usecase/FindPostsTimelineUsecase.kt
+++ sns-feed-service-v2/application/query-api/src/main/kotlin/com/hyoseok/usecase/FindPostsTimelineUsecase.kt
@@ -5,14 +5,19 @@ import com.hyoseok.exception.QueryApiRateLimitException
 import com.hyoseok.feed.dto.FeedDto
 import com.hyoseok.feed.service.FeedRedisReadService
 import com.hyoseok.follow.service.FollowReadService
+import com.hyoseok.mapper.PostCacheDtoMapper
 import com.hyoseok.mapper.PostDtoMapper
 import com.hyoseok.post.dto.PostCacheDto
 import com.hyoseok.post.dto.PostDto
 import com.hyoseok.post.service.PostReadService
 import com.hyoseok.post.service.PostRedisReadService
+import com.hyoseok.post.service.PostRedisService
 import com.hyoseok.util.PageByPosition
 import com.hyoseok.util.PageRequestByPosition
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 
@@ -21,6 +26,7 @@ class FindPostsTimelineUsecase(
     private val feedRedisReadService: FeedRedisReadService,
     private val followReadService: FollowReadService,
     private val postRedisReadService: PostRedisReadService,
+    private val postRedisService: PostRedisService,
     private val postReadService: PostReadService,
 ) {
 
@@ -52,16 +58,33 @@ class FindPostsTimelineUsecase(
         val postIds: List<Long> = feedDtos.map { it.postId }
         val postCacheDtos: List<PostCacheDto> = postRedisReadService.findPostCaches(ids = postIds)
         val postDtos: List<PostDto> =
-            if (postCacheDtos.isNotEmpty()) {
+            if (postCacheDtos.isNotEmpty() && postCacheDtos.size == pageRequestByPosition.size.toInt()) {
                 postCacheDtos.map { PostDtoMapper.of(postCacheDto = it) }
             } else {
-                postReadService.findPosts(ids = postIds)
+                findPostAndCreatePostCache(postCacheDtos, postIds)
             }
 
         return PageByPosition(
             items = postDtos,
             nextPageRequestByPosition = pageRequestByPosition.next(itemSize = postDtos.size),
         )
+    }
+
+    private fun findPostAndCreatePostCache(
+        postCacheDtos: List<PostCacheDto>,
+        postIds: List<Long>,
+    ): List<PostDto> {
+        val postIdMapByPostCacheDtos: Map<Long, Boolean> = postCacheDtos.associate { it.id to true }
+        val postDtos: List<PostDto> = postReadService.findPosts(ids = postIds)
+        val createPostCacheDto: List<PostCacheDto> = postDtos
+            .filter { postIdMapByPostCacheDtos[it.id] == null || postIdMapByPostCacheDtos[it.id] == false }
+            .map { PostCacheDtoMapper.of(postDto = it) }
+
+        CoroutineScope(context = Dispatchers.IO).launch {
+            createPostCacheDto.forEach { postRedisService.createOrUpdate(dto = it) }
+        }
+
+        return postDtos
     }
 
     private fun fallbackExecute(exception: Exception): PageByPosition<PostDto> {
