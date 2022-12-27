0cabfa33549a09a516f38036bd6f92c5d32ad1d1
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-27 22:52:40 +0900

    feat: Feed 캐시에 만료 시간 제거

--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/feed/repository/FeedRedisTransactionRepositoryImpl.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/feed/repository/FeedRedisTransactionRepositoryImpl.kt
@@ -1,14 +1,13 @@
 package com.hyoseok.feed.repository
 
-import com.hyoseok.feed.entity.Feed
-import com.hyoseok.feed.entity.Feed.Companion.ZSET_FEED_MAX_LIMIT
+import com.hyoseok.feed.entity.FeedCache.Companion.ZSET_FEED_MAX_LIMIT
+import com.hyoseok.feed.entity.FeedCache.Companion.getMemberIdFeedsKey
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
 import java.sql.Timestamp
 import java.time.LocalDateTime
-import java.util.concurrent.TimeUnit.SECONDS
 
 @Repository
 @ConditionalOnProperty(prefix = "spring.feed.redis", name = ["enable"], havingValue = "true")
@@ -25,11 +24,10 @@ class FeedRedisTransactionRepositoryImpl(
             try {
                 redisConnection.multi()
 
-                val (key: String, expireTime: Long) = Feed.getMemberIdFeedsKeyAndExpireTime(id = memberId)
+                val key: String = getMemberIdFeedsKey(id = memberId)
 
                 feedRedisRepository.zadd(key = key, value = postId, score = score)
                 feedRedisRepository.zremRangeByRank(key = key, start = ZSET_FEED_MAX_LIMIT, end = ZSET_FEED_MAX_LIMIT)
-                redisTemplate.expire(key, expireTime, SECONDS)
 
                 redisConnection.exec()
                 return@execute
