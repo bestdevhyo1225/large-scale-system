002c5afd62561f2e7dffcbfd837bab9566cae397
janghyoseog <bestdevhyo1225@gmail.com>
2022-12-31 22:06:57 +0900

    feat: Post 캐시 만료 시간 수정

--- sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/entity/PostCache.kt
+++ sns-feed-service-v2/domain/redis/src/main/kotlin/com/hyoseok/post/entity/PostCache.kt
@@ -18,8 +18,8 @@ data class PostCache(
         private const val HASH_MAX_ENTRIES = 1_000L
 
         const val POST_MEMBER_MAX_LIMIT = -10_001L
-        const val POST_CACHE_EXPIRE_TIME = 60L * 30L // 30분
-        const val POST_IDS_CACHE_BY_MEMBER_ID_EXPIRE_TIME = 60L * 60L * 24 * 30L // 30일
+        const val POST_CACHE_EXPIRE_TIME = 60L * 60L * 12L // 12시간
+        const val POST_IDS_CACHE_BY_MEMBER_ID_EXPIRE_TIME = 60L * 60L * 24L * 30L // 30일
 
         fun getPostBucketKey(id: Long) = "post:bucket:${id.div(HASH_MAX_ENTRIES)}"
         fun getPostViewBucketKey(id: Long) = "post:view:bucket:${id.div(HASH_MAX_ENTRIES)}"
